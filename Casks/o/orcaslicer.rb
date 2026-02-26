fedd8e4c1ffef2d4b8225f73258e23c31ff0bd90
AlexGadd <gadda023@gmail.com>
2026-02-27 06:42:30 +1300

    Implement livecheck for Orca Slicer
    Add livecheck block to Orca Slicer cask for version checking.

--- Casks/o/orcaslicer.rb
+++ Casks/o/orcaslicer.rb
@@ -6,7 +6,20 @@ cask "orcaslicer" do
   name "Orca Slicer"
   desc "G-code generator for 3D printers"
   homepage "https://github.com/SoftFever/OrcaSlicer"
+  
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)[._-]v?(\d+(?:\.\d+)+)$/i)
+    strategy :git do |tags, regex|
+      tags.filter_map do |tag|
+        match = tag.match(regex)
+        next if match.blank?
 
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  end
+  
   conflicts_with cask: "orcaslicer@nightly"
 
   app "OrcaSlicer.app"
